b2af75bdbbd3d157f87b1984b93a674d11888410
ALeonoff80 <a.leonov80@gmail.com>
2017-01-16 20:16:27 +0500

    2017-01-16 before release

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// sass compile
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var prefix = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+	gulp.src('css/main.scss')
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(prefix({browsers: ["> 1%", "ie 8", "Firefox ESR"]}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(function(f) {
+			return f.base;
+		}))
+});
+
+gulp.task('default', ['sass'], function() {
+	gulp.watch('css/main.scss', ['sass']);
+})
+
+/*
+	outputStyle types:
+		nested
+		compact
+		expanded
+		compressed
+*/
